17b5fdc8ada2b536550dcd9d57e660d24280550e
Casey <dinnerdoggy@gmail.com>
2024-07-27 13:46:29 -0500

    matching cards to design template

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ const renderDom = (array) => {
         <div class="card mb-3" style="max-width: 540px;">
             <div class="row g-0">
                 <div class="col-md-4">
-                    <img src="${student.imageUrl}" class="img-fluid rounded-start" alt="image of student">
+                    
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
