6f4015e89a2c33151d22b1a2a3c417603bb3bc37
Casey <dinnerdoggy@gmail.com>
2024-07-27 11:40:46 -0500

    setting up initial bootsrap structure

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log("connected")
+const students = [
+
+]
+
+
+const domString1 = document.querySelector("#admitted")
+
+const renderDom = (students) => {
+    domString.innerHTML = ""; // Clear the existing content
+    students.forEach(students => {
+      domString1.innerHTML += `
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="..." class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      `;
+    });
+  };
+  
+  renderDom()
